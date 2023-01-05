f9abe1877be9d6bd6a96c6ee2ca69a03fbc01ba9
tsriharsha07 <tsriharsha07@gmail.com>
2023-01-05 13:32:08 +0530

    intial commit

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const app = require("./app");
 const connectDatabase = require("./config/database");
 
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: "backend/config/config.env" });
-}
+// if (process.env.NODE_ENV !== "PRODUCTION") {
+//   require("dotenv").config({ path: "backend/config/config.env" });
+// }
 
-// dotenv.config({ path: "backend/config/config.env" });
+dotenv.config({ path: "backend/config/config.env" });
 
 connectDatabase();
 
